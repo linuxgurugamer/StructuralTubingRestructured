462ec11b9a0212bd246ee917eea4cda516222495
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-03-16 14:32:38 -0400

    build scripts

---
+++ buildit.bat
@@ -0,0 +1,36 @@
+@echo off
+
+set RELEASEDIR=d:\Users\jbb\release
+set ZIP="c:\Program Files\7-zip\7z.exe"
+
+
+copy StructuralTubingRestructured.version GameData\DKSalvage
+copy StructuralTubingRestructured.version a.version
+
+set VERSIONFILE=a.version
+rem set /p VERSION= "Enter version: "
+
+rem The following requires the JQ program, available here: https://stedolan.github.io/jq/download/
+c:\local\jq-win64  ".VERSION.MAJOR" %VERSIONFILE% >tmpfile
+set /P major=<tmpfile
+
+c:\local\jq-win64  ".VERSION.MINOR"  %VERSIONFILE% >tmpfile
+set /P minor=<tmpfile
+
+c:\local\jq-win64  ".VERSION.PATCH"  %VERSIONFILE% >tmpfile
+set /P patch=<tmpfile
+
+c:\local\jq-win64  ".VERSION.BUILD"  %VERSIONFILE% >tmpfile
+set /P build=<tmpfile
+del tmpfile
+set VERSION=%major%.%minor%.%patch%
+if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
+
+echo %VERSION%
+del a.version
+
+
+set FILE="%RELEASEDIR%\StructuralTubingRestructured-%VERSION%.zip"
+IF EXIST %FILE% del /F %FILE%
+%ZIP% a -tzip %FILE% GameData
+
